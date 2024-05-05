82a42d6928fefd03956c0ecc6a80dbd5c48723e6
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-05 10:24:25 +0530

    stored creds in dotenv

--- server.js
+++ server.js
@@ -3,27 +3,41 @@ const express=require('express');
 const app=express();
 const bcrypt=require('bcrypt')
 const cors=require('cors')
-const db=[];
+const mongoose=require('mongoose')
+const {Schema, model}=mongoose
+mongoose.connect(process.env.MONGO_URI)
 app.use(cors())
 app.use(express.json())
 app.use(urlencoded({extended:true}))
 
-app.get('/users',(req,res)=>{
-    res.json(db.map((user)=>user.name))
+const UserSchema=new Schema({name:String,password:String})
+const db=model('db',UserSchema)
+
+
+app.get('/users',async (req,res)=>{
+   const user=await db.find({},{name:1})
+    res.json(user)
 });
 
 app.post('/sign-up',async (req,res)=>{
     const salt=await bcrypt.genSalt()
     const hashedPassword=await bcrypt.hash(req.body.password,salt)
-    db.push({name:req.body.username,password:hashedPassword});
+    if(await db.findOne({name:req.body.username})){
+    res.send('<h1>User name already exists</h1>')
+    res.status(400)
+    }
+    else{
+    const user=new db({name:req.body.username,password:hashedPassword});
+    await user.save();
     res.status(201);
     res.send('<h1>User Created</h1>')
+    }
 });
 
 app.post('/login',async (req,res)=>{
   const password=req.body.password;
   const name=req.body.username;
-  const user=db.find((user)=>user.name==name)
+  const user=await db.findOne({name:name})
   if(user==null)
   res.send('<h1>User does not exist</h1>')
   try{
