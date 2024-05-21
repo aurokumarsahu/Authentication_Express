193583ae1b3041ea73f799d26596e5bf7783cdfe
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-21 21:14:11 +0530

    schema key 'name' changed to 'username'

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 
 mongoose.connect(process.env.MONGO_URI)
 app.use(cors())
@@ -14,19 +14,19 @@ app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
 //define mongoose schema and create a model(collection)
-const UserSchema = new Schema({ name: String, password: String })
+const UserSchema = new Schema({ username: String, password: String })
 const db = model('db', UserSchema)
 
 //Sign Up endpoint
 app.post('/sign-up', async (req, res) => {
   const salt = await bcrypt.genSalt()
   const hashedPassword = await bcrypt.hash(req.body.password, salt)
-  if (await db.findOne({ name: req.body.username })) {
+  if (await db.findOne({ username: req.body.username })) {
     res.send('User name already exists')
     res.status(400)
   }
   else {
-    const user = new db({ name: req.body.username, password: hashedPassword });
+    const user = new db({ username: req.body.username, password: hashedPassword });
     await user.save();
     res.status(201).send('User Created successfully');
   }
@@ -35,18 +35,19 @@ app.post('/sign-up', async (req, res) => {
 //Login endpoint
 app.post('/login', async (req, res) => {
   const password = req.body.password;
-  const name = req.body.username;
-  const user = await db.findOne({ name: name })
-  if (user == null)
-    {
-      res.status(400).send('User does not exist')
-    }
+  const username = req.body.username;
+  const user = await db.findOne({ username: username })
+  if (user == null) {
+    res.status(400).send('User does not exist')
+  }
   try {
     const passwordCheck = await bcrypt.compare(password, user.password);
     if (!passwordCheck)
       res.status(401).send('Invalid login')
-    else
+    else {
       res.status(200).send('Logged in successfully')
+      jwt.sign(process.env.JWT_TOKEN,{username:})
+    }
   }
   catch {
     res.status(500);
